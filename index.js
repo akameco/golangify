8ef15628a12bc23798d2636dfd8ffa4d19a383b6
akameco <akameco.t@gmail.com>
2019-02-01 22:07:51 +0900

    fix(deps): update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const handler = {
   apply(target, prop, args) {
     const result = Reflect.apply(target, prop, args)
     if (result.then) {
-      return result.then(v => [v, null]).catch(err => [null, err])
+      return result.then(v => [v, null]).catch(error => [null, error])
     }
     if (result instanceof Error) {
       return [null, result]
